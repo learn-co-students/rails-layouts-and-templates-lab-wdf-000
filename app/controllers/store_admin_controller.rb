e38e4d670dd9235142cfeedcb5ff4be49d705dc9
ozPop <ozziepopovas@gmail.com>
2016-09-19 13:17:58 -0400

    add order layout, view, update admin controller

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,5 +1,9 @@
 class StoreAdminController < ApplicationController
+  layout "admin"
   def home
-    render :layout => "admin"
+  end
+
+  def orders
+    render :layout => "order_administration"
   end
 end
